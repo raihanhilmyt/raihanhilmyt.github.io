78cafabd875da5d0481bec4577a9661f07c7f99b
Raihan Hilmy <raihanhilmy@gmail.com>
2025-04-10 21:37:38 +0200

    Korrektur.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/',
-  plugins: [vue()]
+  plugins: [vue()],
+  base: '/raihanhilmyt.github.io/', // <-- GANZ WICHTIG!! Beispiel: '/raihanhilmyt.github.io/' oder '/portfolio/'
 })
